7059ad6260e841dca2615d988f25a0fc688d02b3
jake matillano <jakematillano.z@gmail.com>
2022-08-24 12:13:56 -0700

    phase1 done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,11 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+// ...
+const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
+// ...
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
