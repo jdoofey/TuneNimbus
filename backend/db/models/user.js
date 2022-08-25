426fbf30d693a2a1395a2c136eca215e85c2612e
jake matillano <jakematillano.z@gmail.com>
2022-08-25 14:11:26 -0700

    model validations added

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -13,13 +13,56 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          }
+        }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      totalAlbums: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      totalSongs: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      previewImg: {
+        type: DataTypes.STRING
+        //might want to set a default value
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
+        }
+      }
+    }
+  );
   return User;
 };
