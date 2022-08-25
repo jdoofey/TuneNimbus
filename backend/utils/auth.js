d9c9416cdf69849b4c4d6011176587c3213b75ed
jake matillano <jakematillano.z@gmail.com>
2022-08-25 14:36:42 -0700

    setTokenCookie added into auth.js

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -4,3 +4,25 @@ const { jwtConfig } = require('../config');
 const { User } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
+
+// Sends a JWT Cookie
+const setTokenCookie = (res, user) => {
+  // Create the token.
+  const token = jwt.sign(
+    { data: user.toSafeObject() },
+    secret,
+    { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+  );
+
+  const isProduction = process.env.NODE_ENV === "production";
+
+  // Set the token cookie
+  res.cookie('token', token, {
+    maxAge: expiresIn * 1000, // maxAge in milliseconds
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction && "Lax"
+  });
+
+  return token;
+};
