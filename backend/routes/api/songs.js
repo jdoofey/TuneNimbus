5290e1d39a10ba8a557815b58489b28004b17a98
jake matillano <jakematillano.z@gmail.com>
2022-08-30 10:30:22 -0700

    songbyid and error handling working

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -9,11 +9,24 @@ router.get('/', async (req, res) => {
   res.json(songs)
 })
 
-router.get('/:userId', async (req, res) => {
-  const songsByUser = await Song.findAll({
-    where:{userId:req.params.userId}
-  })
-  res.json(songsByUser)
+// router.get('/:userId', async (req, res) => {
+//   const songsByUser = await Song.findAll({
+//     where:{userId:req.params.userId}
+//   })
+//   res.json(songsByUser)
+// })
+
+router.get('/:songId', async (req, res) => {
+
+  const songById =  await Song.findByPk(req.params.songId)
+  if (!songById) {
+    res.statusCode = 404
+    res.json({
+      message: 'Song couldn\'t be found',
+      statusCode: res.statusCode
+    })
+  }
+  res.json(songById)
 })
 
 module.exports = router;
