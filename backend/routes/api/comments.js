29d1c82579160c0b6685d8aa65c4b7fda9bbd882
jake matillano <jakematillano.z@gmail.com>
2022-08-30 15:43:48 -0700

    added comments router

---
+++ backend/routes/api/comments.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { User, Song, Album, Comment } = require('../../db/models');
+const router = express.Router();
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const songsRouter = require('./songs.js')
 const playlistsRouter = require('./playlists.js')
+const commentsRouter = require('./comments.js')
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
@@ -19,6 +20,8 @@ router.use('/users', usersRouter);
 router.use('/songs', songsRouter)
 
 router.use('/playlists', playlistsRouter)
+
+router.use('/comments', commentsRouter)
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
