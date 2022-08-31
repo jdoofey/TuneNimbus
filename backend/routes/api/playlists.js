3528be8f67731f7564c3c873b2adf92bb244b2a6
jake matillano <jakematillano.z@gmail.com>
2022-08-30 21:05:04 -0700

    working feature

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
-const { User, Song, Album, Playlist } = require('../../db/models');
+const { User, Song, Album, Playlist, PlaylistSong } = require('../../db/models');
 const router = express.Router();
 
 //this route is unnecessary :c
@@ -8,6 +8,46 @@ router.get('/', async (req, res)=>{
   const playlists = await Playlist.findAll({})
   res.json({playlists})
 })
+
+router.post('/:playlistId/songs', restoreUser,requireAuth,async (req, res)=> {
+  const {playlistId} = req.params
+  const {songId} = req.body
+  const {user:{id}} = req
+  const playlist = await Playlist.findByPk(playlistId)
+  if(!playlist) {
+    res.statusCode = 404
+    res.json({
+      message:'Playlist couldn\'t be found',
+      statusCode: res.statusCode,
+    })
+  }
+  const song = await Song.findByPk(songId)
+  if(!song) {
+    res.statusCode = 404
+    res.json({
+      message:'Song couldn\'t be found',
+      statusCode: res.statusCode,
+    })
+  }
+  if(playlist.userId!==id) {
+    res.statusCode = 401
+    res.json({
+      statusCode: res.statusCode,
+      message: 'Unauthorized'
+    })
+  }
+  await playlist.addSong(song)
+  const playlistSong = await PlaylistSong.findOne({
+    where:{playlistId, songId},
+    attributes:{
+      include:['id','playlistId','songId'],
+      exclude:['createdAt','updatedAt']
+    }
+  })
+
+  res.json(playlistSong)
+})
+
 
 router.post('/', restoreUser, requireAuth, async (req, res)=>{
   const userId = req.user.id
