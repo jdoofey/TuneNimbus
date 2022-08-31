28f8e0fd1c8a0a01975650d0eb8b3172baceb737
jake matillano <jakematillano.z@gmail.com>
2022-08-31 14:02:07 -0700

    comments by song id working

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { User, Song, Album, Comment } = require('../../db/models');
 const router = express.Router();
 
+//coments by song id
+router.get('/:songId/comments', async(req, res)=>{
+  const {songId} = req.params
+  const song = await Song.findByPk(songId)
+  const comments = await song.getComments({
+    include:[{model:User, attributes:['id', 'username']}]
+  })
+  if (!song) {
+    res.statusCode = 404
+    res.json({
+      message: 'Song couldn\'t be found',
+      statusCode: res.statusCode,
+    })
+  }
+  res.json({Comments:comments})
+})
 
 module.exports = router;
