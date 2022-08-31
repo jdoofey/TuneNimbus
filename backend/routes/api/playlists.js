91423d083b456f97016156111393718b463bf2c4
jake matillano <jakematillano.z@gmail.com>
2022-08-30 20:14:02 -0700

    create playlist

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -3,30 +3,31 @@ const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth')
 const { User, Song, Album, Playlist } = require('../../db/models');
 const router = express.Router();
 
+//this route is unnecessary :c
 router.get('/', async (req, res)=>{
   const playlists = await Playlist.findAll({})
   res.json({playlists})
 })
 
-router.post('/', requireAuth, async (req, res)=>{
-  const {name, imageUrl} = req.body
-  try{
-
-    const newPlaylist = await Playlist.create({
-      name, imageUrl
-    })
-    return res.json(newPlaylist)
-  }
-  catch{
+router.post('/', restoreUser, requireAuth, async (req, res)=>{
+  const userId = req.user.id
+  const {name, previewImage} = req.body
+  const playlist = await Playlist.create({name, userId, previewImage})
+  if (!name || name===undefined || name===null) {
     res.statusCode = 400
     res.json({
       message:'Validation Error',
       statusCode: res.statusCode,
       errors: {
-         "name": "Playlist name is required",
+         name: "Playlist name is required",
       }
     })
   }
+  else {
+    res.status(201)
+    res.json({playlist})
+  }
 })
+
 
 module.exports = router;
