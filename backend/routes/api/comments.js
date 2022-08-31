875d8544dd02d92ab512ba7cd2890472bd9c306c
jake matillano <jakematillano.z@gmail.com>
2022-08-31 14:11:35 -0700

    create comment working

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -5,6 +5,25 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { User, Song, Album, Comment } = require('../../db/models');
 const router = express.Router();
 
+//create comment for song via songId
+router.post('/:songId/comments', async (req, res)=>{
+  const {songId} = req.params
+  const {user} = req
+  const {body} = req.body
+  const song = await Song.findByPk(songId)
+  const comment = await Song.createComment({
+    userId: user.id,
+    body
+  })
+  if (!song) {
+    res.statusCode = 404
+    res.json({
+      message: 'Song couldn\'t be found',
+      statusCode: res.statusCode,
+    })
+  }
+  res.json(comment)
+})
 //coments by song id
 router.get('/:songId/comments', async(req, res)=>{
   const {songId} = req.params
