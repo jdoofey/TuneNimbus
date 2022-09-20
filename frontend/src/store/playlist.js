a850dfd71c63d1cd1581eddb9b891cf59721bb12
jake matillano <jakematillano.z@gmail.com>
2022-09-20 14:17:10 -0700

    actions added

--- frontend/src/store/playlist.js
+++ frontend/src/store/playlist.js
@@ -1,7 +1,20 @@
 import { csrfFetch } from "./csrf";
 
-const LOAD_ALL = "albums/LOAD_ALL"
-const ADD_ONE = "albums/ADD_ONE"
-const DELETE_ONE = "albums/DELETE_ONE"
+const LOAD_CURRENT = "playlists/LOAD_CURENT"
+const ADD_ONE = "playlists/ADD_ONE"
+const DELETE_ONE = "playlists/DELETE_ONE"
 
+const loadCurrent = (playlists) => ({
+  type: LOAD_CURRENT,
+  playlists
+})
 
+const addPlaylist = playlist => ({
+  type: ADD_ONE,
+  playlist
+})
+
+const deletePlaylist = playlistId => ({
+  type: DELETE_ONE,
+  playlistId
+})
