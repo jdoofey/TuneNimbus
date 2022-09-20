7c49c2b3a8b957bb0e63ab564d5ded12ac14cdb7
jake matillano <jakematillano.z@gmail.com>
2022-09-20 14:26:52 -0700

    remove playlist thunk

--- frontend/src/store/playlist.js
+++ frontend/src/store/playlist.js
@@ -1,50 +1,58 @@
 import { csrfFetch } from "./csrf";
 
-const LOAD_CURRENT = "playlists/LOAD_CURENT"
-const ADD_ONE = "playlists/ADD_ONE"
-const DELETE_ONE = "playlists/DELETE_ONE"
-const RESET_PLAYLISTS = "playlists/RESET_PLAYLISTS"
+const LOAD_CURRENT = "playlists/LOAD_CURENT";
+const ADD_ONE = "playlists/ADD_ONE";
+const DELETE_ONE = "playlists/DELETE_ONE";
+const RESET_PLAYLISTS = "playlists/RESET_PLAYLISTS";
 
 const loadCurrent = (playlists) => ({
   type: LOAD_CURRENT,
-  playlists
-})
+  playlists,
+});
 
-const addPlaylist = playlist => ({
+const addPlaylist = (playlist) => ({
   type: ADD_ONE,
-  playlist
-})
+  playlist,
+});
 
-const deletePlaylist = playlistId => ({
+const deletePlaylist = (playlistId) => ({
   type: DELETE_ONE,
-  playlistId
-})
+  playlistId,
+});
 
-export const resetPlaylists = () => ({
+export const resetPLaylists = () => ({
   type: RESET_PLAYLISTS,
-})
+});
 //use to clean up stale state
 
-
-
-export const getPlaylistsByCurrentUser = () => async dispatch =>{
-  const res = await csrfFetch("/api/playlists/current")
+export const getPlaylistsByCurrentUser = () => async (dispatch) => {
+  const res = await csrfFetch("/api/playlists/current");
   if (res.ok) {
-    const data = res.json()
-    dispatch(loadCurrent(data))
+    const data = res.json();
+    dispatch(loadCurrent(data));
     //return data
   }
-}
+};
 
-export const createPlaylist = playlist => async dispatch => {
-  const res = await csrfFetch("/api/songs/", {
+export const createPlaylist = (playlist) => async (dispatch) => {
+  const res = await csrfFetch("/api/playlists", {
     method: "POST",
-    headers: {"Content-Type" : "application/json"},
-    body: JSON.stringify(playlist)
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(playlist),
   });
   if (res.ok) {
-    const data = await res.json()
-    dispatch(addPlaylist(data))
-    return data
+    const data = await res.json();
+    dispatch(addPlaylist(data));
+    return data;
+  }
+};
+
+export const removePlaylist = playlistId => async dispatch => {
+  const res = await csrfFetch(`/api/playlists/${playlistId}`, {
+    method: "DELETE"
+  })
+  if (res.ok) {
+    await dispatch(deletePlaylist(songId))
+    return res
   }
 }
