3522923b5a0577c9cc2e3b0ffbd59908d91489f8
jake matillano <jakematillano.z@gmail.com>
2022-09-20 15:16:58 -0700

    current playlists skeleton

--- frontend/src/components/Playlists/CurrentPlaylists/CurrentPlaylist.js
+++ frontend/src/components/Playlists/CurrentPlaylists/CurrentPlaylist.js
@@ -2,4 +2,14 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPlaylistsByCurrentUser,
          resetPLaylists } from "../../../store/playlist";
+import Playlist from "../SinglePlaylist/SinglePlaylist";
 
+export const CurrentPlaylists = () => {
+  const dispatch = useDispatch()
+  const playlists = useSelector((state)=> state.playlist.allPlaylists)
+
+  useEffect(() => {
+    dispatch(getPlaylistsByCurrentUser())
+    return () => dispatch(resetPLaylists())
+  })
+}
