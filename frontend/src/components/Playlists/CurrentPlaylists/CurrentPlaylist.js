bc8a8b0a9ff0d5b0d3796ef8c7261a0f8bb64943
jake matillano <jakematillano.z@gmail.com>
2022-09-20 15:10:43 -0700

    creating file structure for playlist components

---
+++ frontend/src/components/Playlists/CurrentPlaylists/CurrentPlaylist.js
@@ -0,0 +1,5 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPlaylistsByCurrentUser,
+         resetPLaylists } from "../../../store/playlist";
+
