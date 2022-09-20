d926f6255959c7c5b5f15de16c2cd7925b1ff4b5
jake matillano <jakematillano.z@gmail.com>
2022-09-20 14:20:44 -0700

    get playlists by current user thunk

--- frontend/src/store/playlist.js
+++ frontend/src/store/playlist.js
@@ -3,6 +3,7 @@ import { csrfFetch } from "./csrf";
 const LOAD_CURRENT = "playlists/LOAD_CURENT"
 const ADD_ONE = "playlists/ADD_ONE"
 const DELETE_ONE = "playlists/DELETE_ONE"
+const RESET_PLAYLISTS = "playlists/RESET_PLAYLISTS"
 
 const loadCurrent = (playlists) => ({
   type: LOAD_CURRENT,
@@ -18,3 +19,17 @@ const deletePlaylist = playlistId => ({
   type: DELETE_ONE,
   playlistId
 })
+
+export const resetPlaylists = () => ({
+  type: RESET_PLAYLISTS,
+})
+//use to clean up stale state
+
+export const getPlaylistsByCurrentUser = () => async dispatch =>{
+  const res = await fetch("/api/playlists/current")
+  if (res.ok) {
+    const data = res.json()
+    dispatch(loadCurrent(data))
+    //return data
+  }
+}
