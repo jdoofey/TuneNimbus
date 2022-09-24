65e1bdb16aa9e0e40442d9b262515d3cc6a2e806
jake matillano <jakematillano.z@gmail.com>
2022-09-23 21:26:02 -0700

    styling changes

--- frontend/src/components/Playlists/CurrentPlaylists/CurrentPlaylist.js
+++ frontend/src/components/Playlists/CurrentPlaylists/CurrentPlaylist.js
@@ -1,41 +1,44 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getPlaylistsByCurrentUser,
-         resetPLaylists } from "../../../store/playlist";
+import {
+  getPlaylistsByCurrentUser,
+  resetPLaylists,
+} from "../../../store/playlist";
 import { NavLink } from "react-router-dom";
 import SinglePlaylist from "../SinglePlaylist/SinglePlaylist";
 import CreatePlaylist from "../CreatePlaylist/CreatePlaylist";
-import "./CurrentPlaylist.css"
+import "./CurrentPlaylist.css";
 export const CurrentPlaylists = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-
-  const playlists = useSelector((state)=> state.playlists)
+  const playlists = useSelector((state) => state.playlists);
 
   useEffect(() => {
-    dispatch(getPlaylistsByCurrentUser())
+    dispatch(getPlaylistsByCurrentUser());
     // return () => dispatch(resetPLaylists())
-  }, [dispatch])
+  }, [dispatch]);
 
   if (!playlists) return null; //add loading page
 
   return (
     <div id="your-playlist-master-container">
-      <div className="playlist-header-box-container"><h1>Your Playlists</h1></div>
-      <div className="create-playlist-btn">
-      <CreatePlaylist />
-      </div>
-    <ul id="curr-playlist-container">
-      {Object.values(playlists).map((playlist) => {
-        return (
-          <li id="list-ele" key={playlist.id}>
-            <SinglePlaylist playlist={playlist} />
-            <div>{playlist.name}</div>
-          </li>
-        )
-      })}
-    </ul>
+      <div className="playlist-header-box-container">
+        <h1>Your Playlists</h1>
+        <div id="createplaylistbtn">
+        <CreatePlaylist />
+        </div>
       </div>
 
-  )
-}
+      <ul id="curr-playlist-container">
+        {Object.values(playlists).map((playlist) => {
+          return (
+            <li id="list-ele" key={playlist.id}>
+              <SinglePlaylist playlist={playlist} />
+              <div>{playlist.name}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
