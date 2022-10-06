ba4ba701023100133bbb8f10cb09f85859ea7e39
jake matillano <jakematillano.z@gmail.com>
2022-10-06 15:14:24 -0700

    working on store

---
+++ frontend/src/store/comments.js
@@ -0,0 +1,9 @@
+import { csrfFetch } from "./csrf";
+
+const LOAD_ONE = "comments/LOAD_ONE";
+const LOAD_CURRENT = "comments/LOAD_CURENT";
+const ADD_ONE = "comments/ADD_ONE";
+const DELETE_ONE = "comments/DELETE_ONE";
+const RESET = "comments/RESET";
+
+export const 
