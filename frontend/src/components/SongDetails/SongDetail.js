43e68a2c35b880b83065fceedb18154babc6d9cc
jake matillano <jakematillano.z@gmail.com>
2022-10-08 13:24:40 -0700

    refactoring song details component

--- frontend/src/components/SongDetails/SongDetail.js
+++ frontend/src/components/SongDetails/SongDetail.js
@@ -6,7 +6,7 @@ import { getSongDeets } from "../../store/songs";
 import React, { useEffect } from "react";
 import "./SongDetail.css";
 import EditSongForm from "../EditSongForm/EditSongForm";
-export default function SongDetails() {
+export default function SongDetails({ setAudioUrl }) {
 
   const dispatch = useDispatch();
   const { songId } = useParams();
@@ -17,51 +17,65 @@ export default function SongDetails() {
     dispatch(getSongDeets(songId));
   }, [songId, dispatch]);
 
-  const sessionUser = useSelector(state=> state.session.user)
+  const sessionUser = useSelector(state => state.session.user)
   // if(!sessionUser) {
   //   // TODO ADD CSS
   // }
 
   return (
-    <div>
-      <div id="container-div">
-        <div>
-          <img id="preview"
-          style={{width:"300px",
-                  height:"300px"
+    <div id="banner-container">
+      <div id="banner-top">
+        <div id="banner-top-left">
+          <button
+            id="song-details-play-button"
+            onClick={(e) => {
+              e.preventDefault();
+              setAudioUrl(song.url);
+            }}
+          >test</button>
+          <div id="banner-song-details">
+            <div>{song.title}</div>
+            <div>{song?.Artist?.username}</div>
+          </div>
+        </div>
+      </div>
+      <img id="song-details-image"
+        style={{
+          width: "300px",
+          height: "300px"
         }}
-          src={
-            song.previewImage!==null && song.previewImage!==""
+        src={
+          song.previewImage !== null && song.previewImage !== ""
             ? song.previewImage
             : "https://i.imgur.com/QwtY70m.jpg"
-          }
-          alt="404"
-          ></img>
-        </div>
-
-          <h1>{song.title}</h1>
-          <h3>By: {song?.Artist?.username}</h3>
-          <h4>Album: {song?.Album?.title}</h4>
-          <h4>Description: {song.description}</h4>
-          <h4>
-            Date Uploaded:{"   "}{new Date(song.createdAt).toString().slice(4, 16)}{"  "}
-            {
-              // song.createdAt.toDateString()
-              // song.createdAt.split("T")[0].split("-")[1] +
-              //   "/" +
-              //   song.createdAt.split("T")[0].split("-")[2] +
-              //   "/" +
-              //   song.createdAt.split("T")[0].split("-")[0]
-              // type of check shows createdAt is a string
-              // but .split still returns undefined
-            }
-          </h4>
-          {(sessionUser.username === song?.Artist?.username) && (
-            <div id="edit-song-container">
-              <EditSongForm song={song}/>
-            </div>
-          )}
-      </div>
+        }
+        alt="404"
+      ></img>
     </div>
   );
 }
+
+//   <img id="preview"
+//   style={{width:"300px",
+//           height:"300px"
+// }}
+//   src={
+//     song.previewImage!==null && song.previewImage!==""
+//     ? song.previewImage
+//     : "https://i.imgur.com/QwtY70m.jpg"
+//   }
+//   alt="404"
+//   ></img>
+// </div>
+
+//   <h1>{song.title}</h1>
+//   <h3>By: {song?.Artist?.username}</h3>
+//   <h4>Album: {song?.Album?.title}</h4>
+//   <h4>Description: {song.description}</h4>
+//   <h4>
+//     Date Uploaded:{"   "}{new Date(song.createdAt).toString().slice(4, 16)}{"  "}
+
+//   </h4>
+//   {(sessionUser.username === song?.Artist?.username) && (
+//     <div id="edit-song-container">
+//       <EditSongForm song={song}/>
