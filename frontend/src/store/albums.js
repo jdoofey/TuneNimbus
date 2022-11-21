65538d61a1192d4e38f031c4b1e3b8ba17e31e0d
jake matillano <jakematillano.z@gmail.com>
2022-11-20 16:42:02 -0800

    action creators for albums done

--- frontend/src/store/albums.js
+++ frontend/src/store/albums.js
@@ -8,3 +8,38 @@ const ADD = "albums/ADD"
 const EDIT = "albums/EDIT"
 const DELETE = "albums/DELETE"
 const RESET = "/albums/RESET"
+
+//action creators
+const loadAllAlbums = albums => ({
+  type: LOAD_ALL,
+  albums
+})
+
+const loadCurrentAlbums = albums => ({
+  type: LOAD_CURRENT,
+  albums
+})
+
+const loadOneAlbum = album => ({
+  type: LOAD_ONE,
+  album
+})
+
+const addAlbum = album => ({
+  type: ADD,
+  album
+})
+
+const editAlbum = album => ({
+  type: EDIT,
+  album
+})
+
+const deleteAlbum = album => ({
+  type: DELETE,
+  album
+})
+
+export const resetAlbums = () => ({
+  type: RESET
+})
