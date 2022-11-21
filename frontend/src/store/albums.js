8c7022e7358cfc78f1002e95cacba8c6b4a49c47
jake matillano <jakematillano.z@gmail.com>
2022-11-20 16:38:36 -0800

    action definitions created for albums

---
+++ frontend/src/store/albums.js
@@ -0,0 +1,10 @@
+import { csrfFetch } from "./csrf";
+
+//ACTIONS DEFINITIONS
+const LOAD_ALL = "albums/LOAD_ALL"
+const LOAD_CURRENT = "albums/LOAD_CURRENT"
+const LOAD_ONE = "albums/LOAD_ONE"
+const ADD = "albums/ADD"
+const EDIT = "albums/EDIT"
+const DELETE = "albums/DELETE"
+const RESET = "/albums/RESET"
