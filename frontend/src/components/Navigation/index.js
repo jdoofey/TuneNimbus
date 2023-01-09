37e99b2ec031cb3579ecfadd86c53ce05267c32d
jake matillano <jakematillano.z@gmail.com>
2023-01-08 19:33:13 -0800

    added external links to LinkedIn and Github

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 // frontend/src/components/Navigation/index.js
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import LoginFormModal from "../LoginFormModal";
@@ -9,64 +9,76 @@ import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
-
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-     <div className="container">
-        <NavLink className="nav-link"to="/">
+      <div className="container">
+        <NavLink className="nav-link" to="/">
           <button className="nav-ele">All Music</button>
         </NavLink>
-        <NavLink className="nav-link"to="/albums">
+
+        <NavLink className="nav-link" to="/albums">
           <button className="nav-ele">Albums</button>
         </NavLink>
-        <NavLink  className="nav-link" to="/songs/current">
+        <NavLink className="nav-link" to="/songs/current">
           <button className="nav-ele">Your Music</button>
         </NavLink>
-        <NavLink  className="nav-link" to="/playlists/current">
+        <NavLink className="nav-link" to="/playlists/current">
           <button className="nav-ele">Your Playlists</button>
         </NavLink>
-        <NavLink  className="nav-link" to="/addsong">
+        <NavLink className="nav-link" to="/addsong">
           <button className="nav-ele">Upload</button>
         </NavLink>
 
         <ProfileButton user={sessionUser} />
 
 
-     </div>
+      </div>
     );
   } else {
     sessionLinks = (
       <div className="container">
         <NavLink className="nav-link" to="/songs">
-          <button  className="nav-ele">All Music</button>
+          <button className="nav-ele">All Music</button>
         </NavLink>
         <LoginFormModal />
         <SignupFormModal />
-        </div>
+      </div>
     );
   }
 
   return (
     <>
-    <div id="nav-container">
-      <span>
-        <NavLink to="/songs">
-        <img id="logos" src="https://i.imgur.com/OHysOUL.png"></img>
-        </NavLink>
-      </span>
+      <div id="nav-container">
+        <div className="logo-external-links-div">
+          <NavLink to="/songs">
+            <img id="logos" src="https://i.imgur.com/OHysOUL.png"></img>
+          </NavLink>
+        <a target="_blank" href="https://github.com/jdoofey/TuneNimbus">
+          <button className="github-btn" ></button>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/jake-matillano">
+          <button className="linkedin-btn" ></button>
+        </a>
+        {/* TODO ADD WELLFOUNDED LINK
+        <a target="_blank" href="https://github.com/jdoofey/TuneNimbus">
+          <button className="github-btn" ></button>
+        </a> */}
+        </div >
+
+        <div className="external-links-div">
+        </div>
 
 
         {!sessionUser && (
           <span className="container">
-
-          <NavLink className="nav-link" exact to="/">
-          <button className="nav-ele">Home</button>
-        </NavLink>
-      </span>
-          )}
-          {isLoaded && sessionLinks}
-    </div>
+            <NavLink className="nav-link" exact to="/">
+              <button className="nav-ele">Home</button>
+            </NavLink>
+          </span>
+        )}
+        {isLoaded && sessionLinks}
+      </div>
     </>
   );
 }
